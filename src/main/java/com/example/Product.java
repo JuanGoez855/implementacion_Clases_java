d74f7912af5cadd8e48d8c6978859aaee703a6ea
juan goez <juangoezdj@gmail.com>
2026-02-26 09:43:28 -0500

    feat: implementacion de atributos de instancia y metodos getter

--- src/main/java/com/example/Product.java
+++ src/main/java/com/example/Product.java
@@ -1,5 +1,34 @@
 package com.example;
 
 public class Product {
+    //Atributos de instancia
+    private String id;
+    private String name;
+    private double price;
+    private int stock;
 
+    //metodos getter
+    public String getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public int getStock() {
+        return stock;
+    }
+
+    //metodos setter
+    public void setId(String id) {
+        this.id = id;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setPrice(double price) {
+        
+    }
 }
